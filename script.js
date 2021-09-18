b40bf1ec5225e982d06aee484dace96d33d7c60b
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2021-09-18 12:09:28 +0430

    js ready
    final

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const leftSide = document.querySelector('.left');
+const rightSide = document.querySelector('.right');
+const container = document.querySelector('.container');
 
+const hoverHandler = (hoverside, unHoverside) => {
+  container.classList.add(hoverside);
+  container.classList.remove(unHoverside);
+};
+
+leftSide.addEventListener(
+  'mouseenter',
+  hoverHandler.bind(this, 'hover-left', 'hover-right')
+);
+
+rightSide.addEventListener(
+  'mouseenter',
+  hoverHandler.bind(this, 'hover-right', 'hover-left')
+);
